4782daa9a8eccb5d28553582d2e0fbbc2107677c
eduardojigub <eduardobrunoit@gmail.com>
2023-03-28 17:54:46 -0300

    feat: add isActive on modal buttons

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+interface RadioBoxProps {
+    isActive: boolean;
+}
+
 export const Container = styled.form`
     h2 {
         color: var(--text-title);
@@ -47,3 +51,39 @@ export const Container = styled.form`
         }
     }
 `;
+
+export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.5rem;
+`
+
+export const RadioBox = styled.button<RadioBoxProps>`
+        height: 4rem;
+        border: 1px solid #d7d7d7;
+        border-radius: 0.25rem;
+
+        background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+
+        img {
+            width: 20px;
+            height: 20px;
+        }
+
+        span {
+            display: inline-block;
+            margin-left: 1rem;
+            font-size: 1rem;
+            color: var(--text-title);
+        }
+        transition: border-color 0.2s;
+
+        &:hover {
+            border-color: #aaa;
+        }
+`
