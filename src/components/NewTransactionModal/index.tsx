9ff1761868d06b0bb20a841c0a20c808aced1997
eduardojigub <eduardobrunoit@gmail.com>
2023-03-31 09:28:13 -0300

    feat: create useTransaction hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,11 @@
-import { FormEvent, useState, useContext } from 'react';
+import { FormEvent, useState } from 'react';
 
 import Modal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { TransactionsContext } from '../../context/TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
@@ -20,7 +20,7 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,4 +1,10 @@
-import { createContext, useEffect, useState, ReactNode } from 'react';
+import {
+  createContext,
+  useEffect,
+  useState,
+  ReactNode,
+  useContext,
+} from 'react';
 import { api } from '../services/api';
 
 interface Transaction {
@@ -21,7 +27,7 @@ interface TransactionsContextData {
   createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
@@ -35,7 +41,10 @@ export function TransactionsProvider(props: TransactionsProviderProps) {
   }, []);
 
   async function createTransaction(transaction: TransactionInput) {
-    const response = await api.post('/transactions', transaction);
+    const response = await api.post('/transactions', {
+      ...transaction,
+      createdAt: new Date(),
+    });
     const { transaction: createdTransaction } = response.data;
 
     setTransactions([...transactions, createdTransaction]);
@@ -45,4 +54,10 @@ export function TransactionsProvider(props: TransactionsProviderProps) {
       {props.children}
     </TransactionsContext.Provider>
   );
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
